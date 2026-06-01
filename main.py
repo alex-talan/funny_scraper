52b44e1489334572389a8fffdc63c6a46b651bf6
Alejandro REYES AMARO <alejandro.reyes-amaro@talan.com>
2026-06-01 17:13:41 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class Orchestrator:
+    def __init__(self):
+        pass
+
+    def invoke(self):
+        print("Hello from my funny scraper app :P")
+
+def main():
+    orchestrator = Orchestrator()
+    orchestrator.invoke()
+
+if __name__ == "__main__":
+    main()
